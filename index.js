44331df2b3e37c87abefc90ab09aa2e0f8327318
AnSui22 <ansui218@ewhain.net>
2024-09-26 15:52:54 +0900

    Feat: index.js로 분리

---
+++ index.js
@@ -0,0 +1,9 @@
+import header from "./src/components/header.js";
+import contentHeader from "./src/components/contentHeader.js";
+
+const index = () => {
+  header();
+  contentHeader();
+};
+
+index();
