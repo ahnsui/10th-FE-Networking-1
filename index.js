863e2fc56c943889c41d5bd09a8b5a6b99175e54
AnSui22 <ansui218@ewhain.net>
2024-10-07 13:27:43 +0900

    Feat: 뉴스 롤링바 디자인 구현

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import header from "./src/components/header.js";
+import newsBar from "./src/components/newsBar.js";
 import contentHeader from "./src/components/contentHeader.js";
 import fieldTab from "./src/components/fieldTab.js";
 
 const index = () => {
   header();
+  newsBar();
   contentHeader();
   fieldTab();
 };
--- src/components/newsBar.js
+++ src/components/newsBar.js
@@ -1,3 +1,27 @@
-const newsBar = () => {};
+const newsBar = () => {
+  const newsBar1 = document.getElementById("newsBar1");
+  const newsBar2 = document.getElementById("newsBar2");
+
+  const newsCompany1 = document.createElement("div");
+  newsCompany1.id = "newsCompany";
+  newsCompany1.innerHTML = "연합뉴스";
+
+  const newsCompany2 = document.createElement("div");
+  newsCompany2.id = "newsCompany";
+  newsCompany2.innerHTML = "연합뉴스";
+
+  const newsBarTitle1 = document.createElement("div");
+  newsBarTitle1.id = "newsBarTitle";
+  newsBarTitle1.innerHTML =
+    "[1보] 김기현·안철수·천하람·황교안, 與전대 본경선 진출";
+
+  const newsBarTitle2 = document.createElement("div");
+  newsBarTitle2.id = "newsBarTitle";
+  newsBarTitle2.innerHTML =
+    "[속보] 與최고위원 본경선, 김병민·김용태·김재원·민영삼";
+
+  newsBar1.append(newsCompany1, newsBarTitle1);
+  newsBar2.append(newsCompany2, newsBarTitle2);
+};
 
 export default newsBar;
